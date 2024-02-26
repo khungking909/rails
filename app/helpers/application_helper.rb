a97a4e446a4dd527adaab6229f9cc58b4d2065f6
Huynh Duc Truong <khungking12345@gmail.com>
2024-02-26 20:02:24 +0700

    merge ne

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def full_title page_title = ""
+    default_title = t("default_title")
+    page_title.empty? ? default_title : "#{page_title} | #{default_title}"
+  end
 end
