a525f567386afce00b0d9e0a77d6a2fe26243540
Huynh Duc Truong <khungking12345@gmail.com>
2024-02-28 09:07:10 +0700

    chapter_7

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,30 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
+  def show
+    @user = User.find_by(id: params[:id])
+    return if @user
+
+    flash[:warning] = t("not_found_user")
+    redirect_to(root_path)
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to(user_path(id: @user.id))
+    else
+      render(:new, status: :unprocessable_entity)
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
 end
